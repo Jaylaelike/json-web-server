f10f3d7faa59995918cd637f5c8f670b4f5d28e2
Jaylaelike <smarkwisai@gmail.com>
2022-12-20 22:02:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+
 const port = process.env.PORT || 3001;
+const cors = require('cors');
+
+server.use(
+    cors({
+        origin: true,
+        credentials: true,
+        preflightContinue: false,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    })
+);
+
+server.options('*', cors());
+
 
 server.use(middlewares);
 server.use(router);
-server.listen(port);
+server.listen(port, () => {
+    console.log('JSON Server is running');
+});
